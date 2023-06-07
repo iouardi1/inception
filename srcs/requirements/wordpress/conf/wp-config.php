6888050f7b83323f7a1122a02b6640c715686cec
iouardi1 <ihssaneouardi2@gmail.com>
2023-06-07 18:50:26 +0100

    here

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,1 +1,8 @@
+
 define('DB_NAME', 'mariadb');
+define('DB_USER', 'iouardi');
+define('DB_PASSWORD', 'DB_PASSWORD');
+define('DB_HOST', '0.0.0.0:3307');
+define( 'WP_SITEURL', 'http://iouardi.com/wordpress' );
+<!-- define( 'WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/path/to/wordpress' ); -->
+define( 'WP_HOME', 'http://iouardi.com' );
