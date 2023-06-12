b8941750cee46f6a51a16a24e94f999d0a5e5bf1
iouardi1 <ihssaneouardi2@gmail.com>
2023-06-12 03:33:59 +0100

    create database and a user and root please

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,7 +1,7 @@
 
-#!/bin/sh
+#!/bin/bash
 
-mysql_install_db
+# mysql_install_db
 
 /etc/init.d/mysql start
 
@@ -15,31 +15,32 @@ else
 
 # Set root option so that connexion without root password is not possible
 
-# mysql_secure_installation << _EOF_
+mysql_secure_installation << _EOF_
 
-# Y
-# root4life
-# root4life
-# Y
-# n
-# Y
-# Y
-# _EOF_
+Y
+root4life
+root4life
+Y
+n
+Y
+Y
+_EOF_
 
 #Add a root user on 127.0.0.1 to allow remote connexion 
 #Flush privileges allow to your sql tables to be updated automatically when you modify it
 #mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
 #Create database and user in the database for wordpress
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
 #Import database in the mysql command line
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+mysql -u  root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 
-/etc/init.d/mysql stop
+# /etc/init.d/mysql stop
 
-# exec "$@"
+exec "$@"
+# mysqld --bind-address=0.0.0.0 
